f05ee2d574419f910d49d90c6d0fb309358e092a
victorbern <victorbernardo96@gmail.com>
2024-04-08 22:43:06 -0300

    Arrumando erro do docker compose

---
+++ backend/prisma/migrations/20240409014203_remove_is_admin_from_cliente/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isAdmin` on the `Cliente` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Cliente" DROP COLUMN "isAdmin";
