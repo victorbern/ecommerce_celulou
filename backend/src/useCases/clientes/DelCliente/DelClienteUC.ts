4307248c5d7fac457c739454fd6b94b763145a8f
victorbern <victorbernardo96@gmail.com>
2024-07-11 20:28:54 -0300

    Adicao da remocao de todos os enderecos de cliente ao apagar cliente

--- backend/src/useCases/clientes/DelCliente/DelClienteUC.ts
+++ backend/src/useCases/clientes/DelCliente/DelClienteUC.ts
@@ -1,10 +1,16 @@
 import { AppError } from "../../../errors/AppError";
 import { IClientesRepository } from "../../../repositories/IClientesRepository";
+import { deleteEnderecoUC } from "../../enderecos/DeleteEndereco";
+import { DeleteEnderecoUC } from "../../enderecos/DeleteEndereco/DeleteEnderecoUC";
+import { findEnderecoByClienteUC } from "../../enderecos/FindEnderecoByCliente";
+import { FindEnderecoByClienteUC } from "../../enderecos/FindEnderecoByCliente/FindEnderecoByClienteUC";
 import { IDelClienteRequestDTO, IDelClienteResponseDTO } from "./DelClienteDTO";
 
 export class DelClienteUC {
     constructor(
         private clientesRepository: IClientesRepository,
+        private findEnderecoByClienteUC: FindEnderecoByClienteUC,
+        private deleteEnderecoUC: DeleteEnderecoUC,
     ) {}
 
     async execute(data: IDelClienteRequestDTO): Promise<IDelClienteResponseDTO> {
@@ -14,6 +20,13 @@ export class DelClienteUC {
 
         if (!clienteExists) {
             throw new AppError("Cliente não encontrado!", 404);
+        }
+
+        // Deleta os endereços do cliente
+        const enderecos = await this.findEnderecoByClienteUC.execute({codigoCliente: codigoCliente});
+
+        for (let i in enderecos) {
+            await this.deleteEnderecoUC.execute({codigoEndereco: enderecos[i].codigoEndereco})
         }
 
         await this.clientesRepository.delete(codigoCliente);
--- backend/src/useCases/clientes/DelCliente/index.ts
+++ backend/src/useCases/clientes/DelCliente/index.ts
@@ -1,11 +1,13 @@
 import { postgresClientesRepository } from "../../../repositories/implementations";
 import { inMemoryClientesRepository } from "../../../repositories/in-memory";
+import { deleteEnderecoUC, deleteEnderecoUCTest } from "../../enderecos/DeleteEndereco";
+import { findEnderecoByClienteUC, findEnderecoByClienteUCTest } from "../../enderecos/FindEnderecoByCliente";
 import { DelClienteController } from "./DelClienteController";
 import { DelClienteUC } from "./DelClienteUC";
 
-const delClienteUC = new DelClienteUC(postgresClientesRepository);
+const delClienteUC = new DelClienteUC(postgresClientesRepository, findEnderecoByClienteUC, deleteEnderecoUC);
 const delClienteController = new DelClienteController(delClienteUC);
 
-const delClienteUCTest = new DelClienteUC(inMemoryClientesRepository);
+const delClienteUCTest = new DelClienteUC(inMemoryClientesRepository, findEnderecoByClienteUCTest, deleteEnderecoUCTest);
 
 export { delClienteUC, delClienteController, delClienteUCTest }
