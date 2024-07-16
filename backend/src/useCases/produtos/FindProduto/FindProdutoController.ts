400d0f4847e5f671cb78facb19f035e3c161f016
victorbern <victorbernardo96@gmail.com>
2024-07-16 14:44:50 -0300

    Pequena correcao no controller de FindProduto

--- backend/src/useCases/produtos/FindProduto/FindProdutoController.ts
+++ backend/src/useCases/produtos/FindProduto/FindProdutoController.ts
@@ -4,14 +4,18 @@ import { FindProdutoUC } from "./FindProdutoUC";
 export class FindProdutoController {
     constructor(
         private findProdutoUC: FindProdutoUC
-    ) {}
+    ) { }
 
     async handle(request: Request, response: Response, next: NextFunction): Promise<Response> {
         const codigo = request.params.codigo;
         try {
-            let result = await this.findProdutoUC.execute({codigoProduto: codigo});
+            let result = await this.findProdutoUC.execute({ codigoProduto: codigo });
 
-            return response.status(200).json({ result: result })
+            if (result) {
+                return response.status(200).json({ result: result })
+            }
+
+            return response.status(404).json({ error: "Nenhum produto encontrado!" })
         } catch (error) {
             next(error);
         }
